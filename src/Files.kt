12d2909e6c052d7483d92d42d6feda657bcbb94a
Eslam El-Meniawy <eslam.elmeniawy@gmail.com>
2018-05-13 14:30:08 +0200

    Read from file

--- src/Files.kt
+++ src/Files.kt
@@ -1,3 +1,4 @@
+import java.io.FileReader
 import java.io.FileWriter
 
 /**
@@ -7,16 +8,41 @@ import java.io.FileWriter
  */
 
 fun main(args: Array<String>) {
-    print("Enter text to add: ")
-    val str: String = readLine().toString()
-    writeToFile(str)
+    println("Make choice:\n1- Read\n2- Write\n")
+    val opr = readLine()!!.toInt()
+
+    when (opr) {
+        1 -> readFromFile()
+        2 -> {
+            print("Enter text to add: ")
+            val str: String = readLine().toString()
+            writeToFile(str)
+        }
+    }
 }
 
 fun writeToFile(str: String) {
     try {
-        val fo = FileWriter("test.txt", true)
-        fo.write(str + "\n")
-        fo.close()
+        val fw = FileWriter("test.txt", true)
+        fw.write(str + "\n")
+        fw.close()
+        println("Data saved")
+    } catch (ex: Exception) {
+        println(ex.message)
+    }
+}
+
+fun readFromFile() {
+    try {
+        val fr = FileReader("test.txt")
+        var c: Int?
+
+        do {
+            c = fr.read()
+            print(c.toChar())
+        } while (c != -1)
+
+        fr.close()
     } catch (ex: Exception) {
         println(ex.message)
     }
